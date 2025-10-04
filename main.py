1b8de36420347ab505225adbc442dfe58920323f
nirmal <mcnirmalkumar00@gmail.com>
2025-10-04 21:00:57 +0530

    abilities w popup

--- main.py
+++ main.py
@@ -26,26 +26,43 @@ class pokeGUI:
         self.button.pack(padx=10, pady=10)
 
         self.label_name = tk.Label(self.root, text="") #just declaring label here
-        self.label_name.pack(padx=10,pady=10)
+        self.label_name.pack(padx=10,pady=10, anchor='w')
 
         self.label_type = tk.Label(self.root, text="")
-        self.label_type.pack(padx=10, pady=10)
+        self.label_type.pack(padx=10, pady=10, anchor='w')
+
+        self.label_ability = tk.Label(self.root, text="", cursor='hand2')
+        self.label_ability.pack(padx=10, pady=10, anchor='w')
 
         self.root.mainloop()
 
     def getInfo(self):
-        pokemon_name = self.textbox.get('1.0', tk.END).strip().lower()
-        pokemon_info = self.get_pokemon_info(pokemon_name)
+        self.pokemon_name = self.textbox.get('1.0', tk.END).strip().lower()
+        self.pokemon_info = self.get_pokemon_info(self.pokemon_name)
 
-        pName = pokemon_info['forms'][0]['name']
-        pType = pokemon_info['types'][0]['type']['name']
+        pName = self.pokemon_info['forms'][0]['name']
+        pType = self.pokemon_info['types'][0]['type']['name']
+        pAbility = self.pokemon_info['abilities'][0]['ability']['name']
 
         self.label_name.config(text=f"Name : {pName.title()}", font=('Arial', 20)) #modifying the label per function call
-       
         self.label_type.config(text=f"Type : {pType.title()}", font=('Arial', 20)) #not including self.root here as im just modifying the label?
-        
-        print(f"Name of the Pokemon is : {pName.title()}")
-        print(f"Type of the Pokemon is : {pType.title()}")       
+
+        self.label_ability.config(text=f"Ability : {pAbility.title()}", font=('Arial', 20))
+        self.label_ability.bind("<Button-1>", self.show_ability_info) #binding mb1 to func call
+
+        #print(f"Name of the Pokemon is : {pName.title()}")
+        #print(f"Type of the Pokemon is : {pType.title()}")
+
+    def show_ability_info(self, event):
+        #to get info out of ability, need to request the url inside the abilities key dict    
+        url = self.pokemon_info['abilities'][0]['ability']['url']
+        response = requests.get(url)
+        ability_data = response.json()
+
+        ability_details = ability_data['effect_entries'][1]['effect']
+
+        messagebox.showinfo("Popup", ability_details)   
+
 
     def get_pokemon_info(self, name):
         url = f"{self.base_url}/pokemon/{name}"
