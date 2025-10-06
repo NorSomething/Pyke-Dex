f84220d21a83174bf1198a2732cebe8d1a6557f3
nirmal <mcnirmalkumar00@gmail.com>
2025-10-06 22:07:36 +0530

    ported to customtkinter

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import requests
 import tkinter as tk
 from tkinter import messagebox
 from tkinter import PhotoImage
+import customtkinter as ctk
+
 
 
 class pokeGUI:
@@ -10,36 +12,36 @@ class pokeGUI:
 
         self.base_url = "https://pokeapi.co/api/v2/"
 
-        self.root = tk.Tk()
+        self.root = ctk.CTk()
 
         self.root.geometry('1800x1200')
 
         self.root.title("Poke-GUI")
 
-        self.label = tk.Label(self.root, text="Poke-GUI", font=('Arial', 35))
+        self.label = ctk.CTkLabel(self.root, text="Poke-GUI", font=('Arial', 35))
         self.label.pack(padx=10, pady=10)
 
-        self.textbox = tk.Text(self.root, height=1, font=('Arial', 25))
+        self.textbox = ctk.CTkTextbox(self.root, height=1, font=('Arial', 25))
         self.textbox.pack(padx=200,pady=10)
 
-        self.checkState = tk.IntVar() #check if button is clicked
+        self.checkState = ctk.IntVar() #check if button is clicked
 
-        self.button = tk.Button(self.root, text="Get Information", command=self.getInfo, font=('Arial', 20)) 
+        self.button = ctk.CTkButton(self.root, text="Get Information", command=self.getInfo, font=('Arial', 20)) 
         self.button.pack(padx=10, pady=10)
 
-        self.label_sprite = tk.Label(self.root) 
+        self.label_sprite = ctk.CTkLabel(self.root, text="") 
         self.label_sprite.pack(padx=10, pady=10)
 
-        self.label_name = tk.Label(self.root, text="") #just declaring label here
+        self.label_name = ctk.CTkLabel(self.root, text="") #just declaring label here
         self.label_name.pack(padx=10,pady=10, anchor='w')
 
-        self.label_type = tk.Label(self.root, text="")
+        self.label_type = ctk.CTkLabel(self.root, text="")
         self.label_type.pack(padx=10, pady=10, anchor='w')
 
-        self.label_ability1 = tk.Label(self.root, text="", cursor='hand2')
+        self.label_ability1 = ctk.CTkLabel(self.root, text="", cursor='hand2')
         self.label_ability1.pack(padx=10, pady=10, anchor='w')
 
-        self.label_ability2 = tk.Label(self.root, text="", cursor='hand2')
+        self.label_ability2 = ctk.CTkLabel(self.root, text="", cursor='hand2')
         self.label_ability2.pack(padx=10, pady=10, anchor='w')
 
         
@@ -47,7 +49,7 @@ class pokeGUI:
         self.root.mainloop()
 
     def getInfo(self):
-        self.pokemon_name = self.textbox.get('1.0', tk.END).strip().lower()
+        self.pokemon_name = self.textbox.get('1.0', 'end').strip().lower()
         self.pokemon_info = self.get_pokemon_info(self.pokemon_name)
 
         pName = self.pokemon_info['forms'][0]['name']
@@ -55,13 +57,13 @@ class pokeGUI:
         pAbility1 = self.pokemon_info['abilities'][0]['ability']['name']
         pAbility2 = self.pokemon_info['abilities'][1]['ability']['name']
 
-        self.label_name.config(text=f"Name : {pName.title()}", font=('Arial', 20)) #modifying the label per function call
-        self.label_type.config(text=f"Type : {pType.title()}", font=('Arial', 20)) #not including self.root here as im just modifying the label?
+        self.label_name.configure(text=f"Name : {pName.title()}", font=('Arial', 20)) #modifying the label per function call
+        self.label_type.configure(text=f"Type : {pType.title()}", font=('Arial', 20)) #not including self.root here as im just modifying the label?
 
-        self.label_ability1.config(text=f"Ability 1 : {pAbility1.title()}", font=('Arial', 20))
+        self.label_ability1.configure(text=f"Ability 1 : {pAbility1.title()}", font=('Arial', 20))
         self.label_ability1.bind("<Button-1>", self.show_ability1_info) #binding mb1 to func call
 
-        self.label_ability2.config(text=f"Ability 2 : {pAbility2.title()}", font=('Arial', 20))
+        self.label_ability2.configure(text=f"Ability 2 : {pAbility2.title()}", font=('Arial', 20))
         self.label_ability2.bind("<Button-1>", self.show_ability2_info)
 
         sprite_url = self.pokemon_info['sprites']['front_default']
@@ -90,8 +92,9 @@ class pokeGUI:
     def show_sprites(self, url):
         response = requests.get(url)
         self.sprite_image = tk.PhotoImage(data=response.content)
+        #self.sprite_image = PhotoImage(data=response.content)
         self.sprite_image_resized = self.sprite_image.zoom(3,3) #Xx and Yx (multipliyers), only integer multiplyers
-        self.label_sprite.config(image=self.sprite_image_resized, text="")
+        self.label_sprite.configure(image=self.sprite_image_resized, text="")
 
     def get_pokemon_info(self, name):
         url = f"{self.base_url}/pokemon/{name}"
