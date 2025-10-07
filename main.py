da1914af780e81eb78b13aff837930e819b76c01
nirmal <mcnirmalkumar00@gmail.com>
2025-10-07 17:43:10 +0530

    added frames for grid system

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from tkinter import PhotoImage
 import customtkinter as ctk
 
 
-
 class pokeGUI:
 
     def __init__(self):
@@ -14,35 +13,72 @@ class pokeGUI:
 
         self.root = ctk.CTk()
 
-        self.root.geometry('1800x1200')
+        self.root.geometry('1920x1080')
 
         self.root.title("Poke-GUI")
 
-        self.label = ctk.CTkLabel(self.root, text="Poke-GUI", font=('Arial', 55))
+        #Top Frame -> Title, Textbox and Get Info button
+        #Info Frame -> Name, Type, Abilities    
+        #Sprite Frame -> Sprite
+
+        self.top_frame = ctk.CTkFrame(self.root)
+        self.top_frame.pack(padx=20, pady=20, fill='x')
+
+        self.info_frame = ctk.CTkFrame(self.root)
+        self.info_frame.pack(padx=20, pady=20, fill="both", expand=True)
+
+        #self.sprite_frame = ctk.CTkFrame(self.info_frame)
+        #self.sprite_frame.pack(padx=20, pady=10, fill="x")
+
+        self.label = ctk.CTkLabel(self.top_frame, text="Poke-GUI", font=('Arial', 55))
         self.label.pack(padx=10, pady=10)
 
-        self.textbox = ctk.CTkTextbox(self.root, height=10, font=('Arial', 45))
+        self.textbox = ctk.CTkTextbox(self.top_frame, height=10, font=('Arial', 45))
         self.textbox.pack(padx=10,pady=10)
 
         self.checkState = ctk.IntVar() #check if button is clicked
 
-        self.button = ctk.CTkButton(self.root, text="Get Information", command=self.getInfo, font=('Arial', 30)) 
+        self.button = ctk.CTkButton(self.top_frame, text="Get Information", command=self.getInfo, font=('Arial', 30)) 
         self.button.pack(padx=20, pady=10)
 
-        self.label_sprite = ctk.CTkLabel(self.root, text="") 
+        self.sprite_name_frame = ctk.CTkFrame(self.info_frame)
+        self.sprite_name_frame.pack(padx=10, pady=10, fill='x')
+
+        self.label_sprite = ctk.CTkLabel(self.sprite_name_frame, text="") 
         self.label_sprite.pack(padx=10, pady=10)
 
-        self.label_name = ctk.CTkLabel(self.root, text="") #just declaring label here
-        self.label_name.pack(padx=10,pady=10, anchor='w')
+        self.label_name = ctk.CTkLabel(self.sprite_name_frame, text="") #just declaring label here
+        self.label_name.pack(padx=10,pady=10)
 
-        self.label_type = ctk.CTkLabel(self.root, text="")
+        #self.label_type = ctk.CTkLabel(self.info_frame, text="")
+        #self.label_type.pack(padx=10, pady=10, anchor='w')
+
+        #Lower Frame (has types and abilities)
+        self.lower_frame = ctk.CTkFrame(self.info_frame)
+        self.lower_frame.pack(padx=10, pady=10, fill='x')
+
+        #Type Frame
+        self.type_frame = ctk.CTkFrame(self.lower_frame)
+        self.type_frame.pack(side='left', padx=10, pady=10, fill='both', expand=True)
+
+        self.label_type = ctk.CTkLabel(self.type_frame, text="", font=('Arial', 35))
         self.label_type.pack(padx=10, pady=10, anchor='w')
 
-        self.label_ability1 = ctk.CTkLabel(self.root, text="", cursor='hand2')
-        self.label_ability1.pack(padx=10, pady=10, anchor='w')
+        # Abilities Frame
+        self.ability_frame = ctk.CTkFrame(self.lower_frame)
+        self.ability_frame.pack(side='right', padx=10, pady=10, fill='both', expand=True)
 
-        self.label_ability2 = ctk.CTkLabel(self.root, text="", cursor='hand2')
-        self.label_ability2.pack(padx=10, pady=10, anchor='w')
+        self.label_ability1 = ctk.CTkLabel(self.ability_frame, text="", cursor='hand2')
+        self.label_ability1.grid(row=0, column=0, padx=10, pady=10, sticky='w')
+
+        self.label_ability1_info = ctk.CTkLabel(self.ability_frame, text="", font=('Arial', 25))
+        self.label_ability1_info.grid(row=0, column=1, padx=10, pady=10, sticky='w')
+
+        self.label_ability2 = ctk.CTkLabel(self.ability_frame, text="", cursor='hand2')
+        self.label_ability2.grid(row=1, column=0, padx=10, pady=10, sticky='w')
+
+        self.label_ability2_info = ctk.CTkLabel(self.ability_frame, text="", font=('Arial', 25))
+        self.label_ability2_info.grid(row=1, column=1, padx=10, pady=10, sticky='w')
 
         
 
@@ -57,13 +93,13 @@ class pokeGUI:
         pAbility1 = self.pokemon_info['abilities'][0]['ability']['name']
         pAbility2 = self.pokemon_info['abilities'][1]['ability']['name']
 
-        self.label_name.configure(text=f"Name : {pName.title()}", font=('Arial', 35)) #modifying the label per function call
-        self.label_type.configure(text=f"Type : {pType.title()}", font=('Arial', 35)) #not including self.root here as im just modifying the label?
+        self.label_name.configure(text=f"{pName.title()}", font=('Arial', 25)) #modifying the label per function call
+        self.label_type.configure(text=f"Type : {pType.title()}", font=('Arial', 25)) #not including self.root here as im just modifying the label?
 
-        self.label_ability1.configure(text=f"Ability 1 : {pAbility1.title()}", font=('Arial', 35))
+        self.label_ability1.configure(text=f"Ability 1 : {pAbility1.title()}", font=('Arial', 25))
         self.label_ability1.bind("<Button-1>", self.show_ability1_info) #binding mb1 to func call
 
-        self.label_ability2.configure(text=f"Ability 2 : {pAbility2.title()}", font=('Arial', 35))
+        self.label_ability2.configure(text=f"Ability 2 : {pAbility2.title()}", font=('Arial', 25))
         self.label_ability2.bind("<Button-1>", self.show_ability2_info)
 
         sprite_url = self.pokemon_info['sprites']['front_default']
@@ -77,7 +113,9 @@ class pokeGUI:
 
         ability_details = ability_data['effect_entries'][1]['effect']
 
-        messagebox.showinfo("Popup", ability_details)  
+        #self.label_ability1.configure(text=f"Ability 1 : {self..title()} - {effect}")
+
+        #messagebox.showinfo("Popup", ability_details)  
 
     def show_ability2_info(self, event):
         #to get info out of ability, need to request the url inside the abilities key dict    
@@ -87,13 +125,13 @@ class pokeGUI:
 
         ability_details = ability_data['effect_entries'][1]['effect']
 
-        messagebox.showinfo("Popup", ability_details)  
+        #messagebox.showinfo("Popup", ability_details)  
 
     def show_sprites(self, url):
         response = requests.get(url)
         self.sprite_image = tk.PhotoImage(data=response.content)
         #self.sprite_image = PhotoImage(data=response.content)
-        self.sprite_image_resized = self.sprite_image.zoom(4,4) #Xx and Yx (multipliyers), only integer multiplyers
+        self.sprite_image_resized = self.sprite_image.zoom(3,3) #Xx and Yx (multipliyers), only integer multiplyers
         self.label_sprite.configure(image=self.sprite_image_resized, text="")
 
     def get_pokemon_info(self, name):
